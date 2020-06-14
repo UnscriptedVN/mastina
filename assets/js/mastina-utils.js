963de87c856c2e0747bf99eedf52b8c1a69a4b3f
Marquis Kurt <software@marquiskurt.net>
2020-06-14 12:12:23 -0400

    [templates + assets] Add search box support

--- assets/js/mastina-utils.js
+++ assets/js/mastina-utils.js
@@ -1,18 +1,20 @@
 /**
  * Mastina Utilities
  * (C) 2020 Marquis Kurt. All rights reserved.
- * 
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
 function toggleZip() {
-    let elements = document.getElementsByClassName("ms-navbar-links");
-    console.log(elements);
-    for (let element of elements) {
-        element.classList.contains("ms-navbar-menu-open") 
-            ? element.classList.remove("ms-navbar-menu-open")
-            : element.classList.add("ms-navbar-menu-open")
-    }; 
+  let elements = document.getElementsByClassName(
+    "ms-navbar-mobile_togglable"
+  )[0].children;
+  console.log(elements);
+  for (let element of elements) {
+    element.classList.contains("ms-navbar-menu-open")
+      ? element.classList.remove("ms-navbar-menu-open")
+      : element.classList.add("ms-navbar-menu-open");
+  }
 }
